9064b2b2ff9aee329dc910bc4d6c11ac74ebce49
Alonza0314 <dufengplus@gmail.com>
2025-02-13 10:48:17 +0000

    feat: add mac version build cmd

--- build_cmd.sh
+++ build_cmd.sh
@@ -11,5 +11,7 @@ echo "}" >> ./main/main.go
 
 GOOS=linux GOARCH=amd64 go build -o ./build/logger-go-linux-amd64 ./main/main.go
 GOOS=windows GOARCH=amd64 go build -o ./build/logger-go-windows-amd64.exe ./main/main.go
+GOOS=darwin GOARCH=amd64 go build -o ./build/logger-go-darwin-amd64 ./main/main.go
+GOOS=darwin GOARCH=arm64 go build -o ./build/logger-go-darwin-arm64 ./main/main.go
 
 rm -rf ./main
